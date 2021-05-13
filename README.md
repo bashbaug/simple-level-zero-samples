b6b8c179cb29bb6d5ad66814fdc00368fef47b4c
Ben Ashbaugh <ben.ashbaugh@intel.com>
2021-05-12 22:53:16 -0700

    initial version

---
+++ README.md
@@ -0,0 +1,41 @@
+# Simple Level Zero Samples
+
+#[![build](https://github.com/bashbaug/SimpleOpenCLSamples/workflows/build/badge.svg?branch=master)](https://github.com/bashbaug/SimpleOpenCLSamples/actions?query=workflow%3Abuild+branch%3Amaster)
+
+This repo contains simple Level Zero samples that demonstrate how to build
+Level Zero applications using only the open-source headers and libs.
+All samples have been tested on Windows and Linux.
+
+## Code Structure
+
+```
+README.md               This file
+LICENSE                 License information
+CMakeLists.txt          Top-level CMakefile
+external/               External Projects (headers and libs)
+include/                Include Files
+samples/                Samples
+```
+
+## How to Build the Samples
+
+The samples require the following external dependencies:
+
+Level Zero Headers and Loader:
+
+    git clone https://github.com/oneapi-src/level-zero external/level-zero
+
+After satisfying the external dependencies create build files using CMake.  For example:
+
+    mkdir build && cd build
+    cmake ..
+
+Then, build with the generated build files.
+
+## License
+
+These samples are licensed under the [MIT License](LICENSE).
+
+Notes:
+* The samples use [popl](https://github.com/badaix/popl) for its options
+parsing, which is licensed under the MIT License.
---
+++ samples/00_enumlevelzero/main.cpp
@@ -0,0 +1,115 @@
+/*
+// Copyright (c) 2019-2020 Ben Ashbaugh
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <vector>
+#include <popl/popl.hpp>
+
+#include "ze_api.h"
+
+static void PrintDeviceType(
+    const char* label,
+    ze_device_type_t type )
+{
+    const char* s;
+    switch (type) {
+        case ZE_DEVICE_TYPE_GPU: s = "GPU"; break;
+        case ZE_DEVICE_TYPE_CPU: s = "CPU"; break;
+        case ZE_DEVICE_TYPE_FPGA: s = "FPGA"; break;
+        case ZE_DEVICE_TYPE_MCA: s = "MCA"; break;
+        default: s = "*UNKNOWN!"; break;
+    }
+    printf("%s%s\n", label, s);
+}
+
+int main(
+    int argc,
+    char** argv )
+{
+    {
+        popl::OptionParser op("Supported Options");
+
+        bool printUsage = false;
+        try {
+            op.parse(argc, argv);
+        } catch (std::exception& e) {
+            fprintf(stderr, "Error: %s\n\n", e.what());
+            printUsage = true;
+        }
+
+        if (printUsage || !op.unknown_options().empty() || !op.non_option_args().empty()) {
+            fprintf(stderr,
+                "Usage: enumlevelzero [options]\n"
+                "%s", op.help().c_str());
+            return -1;
+        }
+    }
+
+    ze_result_t result;
+
+    result = zeInit(0);
+    if (result != ZE_RESULT_SUCCESS) {
+        printf("zeInit failed (%u)!\n", result);
+    }
+
+    uint32_t driverCount = 0;
+    result = zeDriverGet(&driverCount, nullptr);
+    printf("Enumerated %u drivers.\n\n", driverCount);
+
+    std::vector<ze_driver_handle_t> drivers(driverCount);
+    result = zeDriverGet(&driverCount, drivers.data());
+
+    for (auto& driver : drivers) {
+        printf("Driver:\n");
+
+        ze_driver_properties_t driverProps = {};
+        driverProps.stype = ZE_STRUCTURE_TYPE_DRIVER_PROPERTIES;
+        zeDriverGetProperties(driver, &driverProps);
+
+        printf("\tDriver Version: %u\n", driverProps.driverVersion );
+
+        uint32_t deviceCount = 0;
+        zeDeviceGet(driver, &deviceCount, nullptr);
+
+        std::vector<ze_device_handle_t> devices(deviceCount);
+        zeDeviceGet(driver, &deviceCount, devices.data());
+
+        for (uint32_t i = 0; i < deviceCount; i++) {
+            printf("Device[%u]:\n", i);
+
+            ze_device_properties_t deviceProps = {};
+            deviceProps.stype = ZE_STRUCTURE_TYPE_DEVICE_PROPERTIES;
+            zeDeviceGetProperties(devices[i], &deviceProps);
+            
+            PrintDeviceType("\ttype:           ", deviceProps.type);
+
+            printf("\tname:           %s\n", deviceProps.name);
+            printf("\tvendorId:       %04X\n", deviceProps.vendorId);
+            printf("\tdeviceId:       %04X\n", deviceProps.deviceId);
+        }
+        printf("\n");
+    }
+
+    printf( "Done.\n" );
+
+    return 0;
+}
